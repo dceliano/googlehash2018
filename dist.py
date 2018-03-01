74fd2d6b385a41f869921ccfaf94675aa747722e
Dom <d.celiano@yahoo.com>
2018-03-01 19:13:15 +0000

    added classes for vehicle and ride and tick loop.

--- dist.py
+++ dist.py
@@ -14,6 +14,31 @@ def distances(start_point, rides):
         dist_map[ride_number] = dist
     return dist_map
 
+class Ride(object):
+    def __init__(self, start_row, start_column, finish_row, finish_column, earliest_start, latest_finish, ride_number):
+        self.start_row = start_row
+        self.start_column = start_column
+        self.finish_row = finish_row
+        self.finish_column = finish_column
+        self.earliest_start = earliest_start
+        self.latest_finish = latest_finish
+        self.ride_number = ride_number
+        
+class Vehicle(object):
+    def __init__(self, busy, vehicle_num):
+        self.busy = 0
+        self.vehicle_num = vehicle_num
+        self.delivered_passengers = []
+        self.passenger_distances = {}
+    def setBusy(busy):
+        self.busy = busy
+    def incrementTime(busy):
+        if self.busy:
+            totalTime
+    def deliverPassenger(passenger_num):
+        self.delivered_passengers.append(passenger_num)
+
+
 filename = sys.argv[1]
 
 file = open(filename, 'r')
@@ -25,7 +50,7 @@ cols = int(params[1])
 n_vehicles = int(params[2])
 n_rides = int(params[3])
 bonus = int(params[4])
-T = int(params[5])
+max_global_time = int(params[5])
 
 rides = []
 ride_number = 0
@@ -40,14 +65,27 @@ for line in file:
     s = int(params[4])
     f = int(params[5])
 
-    ride = {'start_row': a, 'start_col': b, 'finish_row': x, 'finish_col': y, 'earliest_start': s, 'latest_finish': f, 'ride_number' : ride_number}
+    #ride = {'start_row': a, 'start_col': b, 'finish_row': x, 'finish_col': y, 'earliest_start': s, 'latest_finish': f, 'ride_number' : ride_number}
+    ride = Ride(a, b, x, y, s, f, ride_number)
+    rides.append(ride)
+    ride_number += 1
 
-    rides.append(ride) 
-    ride_number = ride_number + 1
+vehicles = []
+for i in range(0, n_vehicles):
+    vehicles.append(Vehicle(0, i + 1))
 
-s = cityblock([1,1], [1,2])
+
+
+s = cityblock([1,1], [1,2]) #example of how to calculate distance between two points
 print(s)
 
-print(rides)
+#print(rides)
+#print(vehicles)
 
-print(distances([0,0], rides))
+#print(distances([0,0], rides))
+
+# Clock is ticking. T is global time.
+for T in range(0, max_global_time):
+    #Do the actual algorithm
+    x = 1
+
